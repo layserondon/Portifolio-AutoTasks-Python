d6f7118ed49f35268b72508529e7ce1945139d9c
Layse Rondon <layserondon00@gmail.com>
2026-08-21 16:09:07 -0300

    Função Atendimentos iniciada

--- Relatorio-Automático/app.py
+++ Relatorio-Automático/app.py
@@ -9,22 +9,40 @@ def faturamento():
         reader = csv.reader(arquivo)
 
         for linha in reader:
-            dados = linha[0].split(',')
+            status = linha[0].split(',')
 
-            if dados[-1] == 'concluido':
-                concluidos.append(dados)
+            if status[-1] == 'concluido':
+                concluidos.append(status)
+        print(f'Foram concluidos: {len(concluidos)}')
     #Pegar o valor do serviço
+    valores = []
+    with open('atendimentos.csv', mode='r', encoding='utf-8') as arquivo:
+        reader = csv.reader(arquivo)
+        for linha in reader:
+            valor = linha[0].split(',')
+
+            if valor[-2]:
+                valores.append(valor[-2])
+        int_list = list(map(int, valores))
+        total = sum(int_list)
+        print(f'Faturamento total: {total}')
 
     return concluidos
+
 faturamento = faturamento()
 print(faturamento)
 
-
-
-# Quantidade de atendimentos > total de serviços realizados, quantos foram concluídos,
+# Quantidade de atendimentos > total de serviços realizados
 # quantos foram cancelados e pendentes
 def atendimentos():
-    pass
+    with open('atendimentos.csv', mode='r', encoding='utf-8') as arquivo:
+        reader = csv.reader(arquivo)
+        #total de servicos realizados
+        realizados = len(list(reader))
+        print(f'Total de serviços agendados: {realizados}')
+
+atendimentos = atendimentos()
+print(atendimentos)
 
 # Serviço mais vendido
 def serviço():
